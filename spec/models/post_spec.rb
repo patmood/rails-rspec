172932f71842f28c4e917c71ec76dccfe6d72dd1
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 14:16:06 -0500

    fixed controller tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 describe Post do
   it "title should be automatically titleized before save" do
-    pending
+    test_title = Post.new(title: "un titleized title", content: "idgaf")
+    test_title.save
+    test_title.title.should == "Un Titleized Title"
   end
 
   it "post should be unpublished by default" do
-    pending
+    test_post = Post.create(title: "un titleized title", content: "idgaf")
+    test_post.is_published.should == false
   end
 
   # a slug is an automaticaly generated url-friendly
